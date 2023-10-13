e60f3b5695450a4bf3b8c88d106a3afa2b2553dc
Dandara <dands20melo@gmail.com>
2023-10-13 16:00:29 -0300

    Menu lateral finalizado sem javascript

---
+++ SideBar/code.js
@@ -0,0 +1,20 @@
+/*
+var pagMenu = document.querySelectorAll('.pag-menu')
+
+function selectLink(){
+    pagMenu.forEach((item)=>
+        item.classList.remove('selecionado')
+    )
+    this.classList.add('selecionado')
+}
+
+pagMenu.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+var btnExp = document.querySelectorAll('#expandir')
+var menuSide = document.querySelectorAll('#side-bar')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})*/
