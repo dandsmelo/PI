508d2d3e17c59d44bb32be6b58808a1a7ec82215
Yasmin <yasminoliveira_santos@hotmail.com>
2023-10-18 13:52:28 -0300

    Remoção btn saber mais

--- Perfil/scriptperfil.js
+++ Perfil/scriptperfil.js
@@ -1,59 +1,60 @@
-//MODAL EDITAR PERFIL
-
+// Função para abrir o modal de edição de perfil
 function openEditModal() {
-    document.getElementById('edit-modal').style.display = 'block';
+  document.getElementById('edit-modal').style.display = 'block';
 }
 
-  function closeEditModal() {
-    document.getElementById('edit-modal').style.display = 'none';
-  }
+// Função para fechar o modal de edição de perfil
+function closeEditModal() {
+document.getElementById('edit-modal').style.display = 'none';
+}
 
-  function updateProfile() {
-    const coverPhotoFile = document.getElementById('cover-photo-file').files[0];
-    const profilePhotoFile = document.getElementById('profile-photo-file').files[0];
-    const userName = document.getElementById('user-name-input').value;
-    const userDescription = document.getElementById('user-description-input').value;
+// Função para atualizar o perfil
+function updateProfile() {
+const coverPhotoFile = document.getElementById('cover-photo-file').files[0];
+const profilePhotoFile = document.getElementById('profile-photo-file').files[0];
+const userName = document.getElementById('user-name-input').value;
+const userDescription = document.getElementById('user-description-input').value;
 
-    if (coverPhotoFile) {
-      const coverPhotoURL = URL.createObjectURL(coverPhotoFile);
-      document.getElementById('ft-capa').src = coverPhotoURL;
-    }
+if (coverPhotoFile) {
+  const coverPhotoURL = URL.createObjectURL(coverPhotoFile);
+  document.getElementById('ft-capa').src = coverPhotoURL;
+}
 
-    if (profilePhotoFile) {
-      const profilePhotoURL = URL.createObjectURL(profilePhotoFile);
-      document.getElementById('foto').src = profilePhotoURL;
-    }
+if (profilePhotoFile) {
+  const profilePhotoURL = URL.createObjectURL(profilePhotoFile);
+  document.getElementById('foto').src = profilePhotoURL;
+}
 
-    document.getElementById('user-name').textContent = userName;
-    document.getElementById('user-description').textContent = userDescription;
+document.getElementById('user-name').textContent = userName;
+document.getElementById('user-description').textContent = userDescription;
 
-    closeEditModal();
-  }
+closeEditModal();
+}
 
-  //MODAL ADICIONAR PROJETO 
+// Função para abrir o modal de adição de projetos
+function openAddProjetos() {
+document.getElementById('add-projetos').style.display = 'block';
+}
 
-  function openAddProjetos() {
-    document.getElementById('add-projetos').style.display = 'block';
-  }
+// Função para fechar o modal de adição de projetos
+function closeEditProjeto() {
+document.getElementById('add-projetos').style.display = 'none';
+}
 
-  function closeEditProjeto() {
-    document.getElementById('add-projetos').style.display = 'none';
-  }
+// Função para atualizar informações do projeto
+function updateProjeto() {
+const nomeProjeto = document.getElementById('nome-projeto').value;
+const descriProjeto = document.getElementById('descri-projeto').value;
+const linkProjeto = document.getElementById('link-projeto').value;
+const coverProjeto = document.getElementById('foto-projeto').files[0];
 
-  function updateProjeto() {
-    const nomeProjeto = document.getElementById('nome-projeto').value;
-    const descriProjeto = document.getElementById('descri-projeto').value;
-    const linkProjeto = document.getElementById('link-projeto').value;
-    const coverProjeto = document.getElementById('foto-projeto').files[0];
+if (coverProjeto) {
+  const coverProjetoURL = URL.createObjectURL(coverProjeto);
+  document.getElementById('img').src = coverProjetoURL;
+}
 
-    if(coverProjeto){
-      const coverProjetoURL= URL.createObjectURL(coverProjeto);
-      document.getElementById('img').src = coverProjetoURL;
-    }
+document.getElementById('nome').textContent = nomeProjeto;
+document.getElementById('descricao').textContent = descriProjeto;
 
-    document.getElementById('nome').textContent = nomeProjeto;
-    document.getElementById('descricao').textContent = descriProjeto;
-    document.getElementById('') //TERMINAR
-
-    closeEditProjeto();
-  }
+closeEditProjeto();
+}
