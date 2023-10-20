8edc23d7b70e0d3b8674390cb901b40c1ec5023c
clau <claudiasantanasilva@yahoo.com.br>
2023-10-20 11:43:48 -0300

    Alterações no blog do aluno

--- BlogAluno1/script2.js
+++ BlogAluno1/script2.js
@@ -39,3 +39,12 @@ function updateCarousel() {
 
 // Definir o primeiro indicador como ativo
 indicators.children[0].classList.add("active");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.querySelector(".search-button");
+
+    searchButton.addEventListener("click", function () {
+        alert("Funcionalidade de pesquisa ainda não implementada.");
+    });
+});
