c3dec8623f7b7d5954bdb5f035bb9bf1a077c73d
marigsiqueira <mgsmariana123@gmail.com>
2023-10-21 17:47:45 -0300

    Redirecionar páginas a partir do perfil

--- Perfil/scriptperfil.js
+++ Perfil/scriptperfil.js
@@ -81,3 +81,24 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+//função redirecionar para o Perfil
+
+function redirecionarPerfil(){
+  var URL= "/Perfil/perfil.html";
+  window.location.href=(URL);
+}
+
+//função redirecionar para o Mural
+
+function redirecionarMural(){
+  var URL = "/MuralDeAvisos/MuralDeAvisos.html";
+  window.location.href = (URL);
+}
+
+//função redirecionar para o Blog
+
+function redirecionarBlog(){
+  var URL = "/BlogAluno1/paginaBlog.html"
+  window.location.href = (URL);
+}
