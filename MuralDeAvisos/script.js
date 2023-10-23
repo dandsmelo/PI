d7c8364b4fb3e1b96d7738d75aae4bc041f0a890
Aluno <Aluno@fatecdiadema.com>
2023-10-23 09:55:25 -0300

    Alinhando div blog aluno

---
+++ MuralDeAvisos/script.js
@@ -0,0 +1,20 @@
+//Redirecionar para o perfil
+
+function redirecionarPerfil(){
+    var URL= "/Perfil/perfil.html";
+    window.location.href=(URL);
+  }
+  
+  //função redirecionar para o Mural
+  
+  function redirecionarMural(){
+    var URL = "/MuralDeAvisos/MuralDeAvisos.html";
+    window.location.href = (URL);
+  }
+  
+  //função redirecionar para o Blog
+  
+  function redirecionarBlog(){
+    var URL = "/BlogAluno1/paginaBlog.html"
+    window.location.href = (URL);
+  }
