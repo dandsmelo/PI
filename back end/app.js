310e579c2d50884e521a1d64a19b0e8f90bd90d7
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2023-11-03 20:39:47 -0300

    Update app.js

--- back end/app.js
+++ back end/app.js
@@ -5,6 +5,25 @@ const rotas = require('./src/routes/index')
 const app = express();
 app.use(cors());
 app.use(express.json());
+const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        return cb(null, "./caminhos/Images")
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/projeto/upload', upload.single('file'), (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+})
 
 app.use('/projeto', rotas)
 
